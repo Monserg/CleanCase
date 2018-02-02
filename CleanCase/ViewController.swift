ee73304bb8fd3fee254465ecf1a8df637785a4da
SerhiiM <msm72@mail.ru>
2018-02-02 09:19:49 +0200

    Create App

---
+++ CleanCase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CleanCase
+//
+//  Created by msm72 on 02.02.2018.
+//  Copyright © 2018 msm72. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
